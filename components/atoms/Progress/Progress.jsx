c246e084445298b69f46b53fdda1a970d368abdf
cristiandev <cristiansotomayor.dev@gmail.com>
2021-05-08 23:40:04 -0500

    progres

--- components/atoms/Progress/Progress.jsx
+++ components/atoms/Progress/Progress.jsx
@@ -1,37 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import styles from './Progress.module.css'
-
 
 const Progress = ({
-  radius, stroke, image, color, percent,
+  radius, stroke, color, percent,
 }) => {
   const [strokeDashoffset, setStrokeDashoffset] = useState(null);
   const [circumference, setCircumference] = useState(null);
   const [normalizedRadius, setNormalizedRadius] = useState(null);
 
   useEffect(() => {
-    // const circumference = this.normalizedRadius * 2 * Math.PI;
-    // const normalizedRadius = radius - stroke * 2;
-    // const strokeDashoffset = this.circumference - progress / 100 * this.circumference;
-
     setNormalizedRadius(radius - stroke * 2);
-    console.log('normalizedRadius', normalizedRadius);
     setCircumference(normalizedRadius * 2 * Math.PI);
-
-    // const strokeDashoffset = this.circumference - progress / 100 * this.circumference;
     setStrokeDashoffset(circumference - percent / 100 * circumference);
-
-    // return () => {
-    //   cleanup
-    // }
-  }, [percent, radius, stroke]);
-
-  console.log('circumference', circumference);
+  }, []);
 
   return (
-    <div className={styles['progress-container']}>
-      <svg
+    <svg
         height={radius * 2}
         width={radius * 2}
         >
@@ -41,26 +25,18 @@ const Progress = ({
           strokeWidth={ stroke }
           strokeDasharray={ circumference + ' ' + circumference }
           style={ { strokeDashoffset } }
-          stroke-width={ stroke }
+          // stroke-width={ stroke }
           r={ normalizedRadius }
           cx={ radius }
           cy={ radius }
           />
-
-        {/* <text x="49%" y="50%" text-anchor="middle" stroke="#4BDBA9" stroke-width="1.5px" dy=".3em">
-          { 'hlola' }
-        </text> */}
-      </svg>
-      <img src={image} alt="" className={styles['progress-image']}/>
-      <text style={{ color: color }} className={styles['progress-percent']}>{ `${percent}%` }</text>
-    </div>
+    </svg>
   );
 };
 
 Progress.propTypes = {
   radius: PropTypes.number.isRequired,
   stroke: PropTypes.number.isRequired,
-  image: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
 };
 
