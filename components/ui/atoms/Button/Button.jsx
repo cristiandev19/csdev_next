37a70c4b44b60b37570ff5661633b6d1675589b7
cristiandev <cristiansotomayor.dev@gmail.com>
2021-06-04 01:41:37 -0500

    agregando estilos con el .module

--- components/ui/atoms/Button/Button.jsx
+++ components/ui/atoms/Button/Button.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './Button.module.css'
 
 const Button = ({
   text,
 }) => {
   return (
     <>
-      <button className="primary-btn">{ text }</button>
+      <button className={styles['primary-btn']}>{ text }</button>
     </>
   )
 }
