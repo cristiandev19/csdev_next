0df963cdcefd53dac88c6d673e75eaa486c1350b
cristiandev <cristiansotomayor.dev@gmail.com>
2021-08-29 14:28:39 -0500

    new milestone section

---
+++ components/ui/organisms/MilestoneSection/MilestoneSection.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './MilestoneSection.module.css';
+
+const MilestoneSection = () => {
+  const urlGnome = 'https://wiki.gnome.org/Hackfests/PeruRumboGSoC2018';
+
+  return (
+    <section className={styles.milestoneSection}>
+      <div>
+        <p>
+          Empece mi carrera como desarrollador de software en un programa llamado "Peru Rumbo GSoC 2018" organizado por LinuxAtUNI del cual llegue a ser el Top 1.
+        </p>
+      </div>
+      <div className="flex-justify">
+        <iframe
+          height="500"
+          width="80%"
+          src={urlGnome}>
+        </iframe>
+      </div>
+    </section>
+
+  )
+};
+
+export default MilestoneSection;
