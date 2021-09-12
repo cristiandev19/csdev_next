2e85637bb20305848b0d6c87be465aaefb739e80
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-12 16:10:59 -0500

    trophy section updated

--- components/TrophyResponsive/TrophyResponsive.tsx
+++ components/TrophyResponsive/TrophyResponsive.tsx
@@ -4,9 +4,9 @@ import useWindowSize, { WidthSizesProps } from '../../hooks/useWindowSize';
 
 const rowColPerSize: WidthSizesProps<{ row: number; col: number }> = {
   extrasmall: { row: 3, col: 2 },
-  small: { row: 3, col: 3 },
-  medium: { row: 3, col: 4 },
-  large: { row: 2, col: 6 },
+  small: { row: 5, col: 1 },
+  medium: { row: 3, col: 3 },
+  large: { row: 2, col: 3 },
   extralarge: { row: 2, col: 6 },
 };
 
