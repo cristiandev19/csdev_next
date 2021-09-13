8c5a220be384ab9875981d2da0252ce37a5dc20e
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-12 21:20:12 -0500

    adding tailwind to next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import '../styles/globals.css';
-import '../styles/button.css';
+// import '../styles/button.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   return <Component {...pageProps} />;
