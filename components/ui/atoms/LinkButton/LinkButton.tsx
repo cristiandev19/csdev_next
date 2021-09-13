3ac4ebc1aaaaa5c8ef5be62c448853869e7b7cf3
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-12 21:19:33 -0500

    update link button

--- components/ui/atoms/LinkButton/LinkButton.tsx
+++ components/ui/atoms/LinkButton/LinkButton.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import styles from './LinkButton.module.css';
-
-{/* <a target="_blank|_self|_parent|_top|framename"> */}
+// import styles from './LinkButton.module.css';
 
 type LinkButtonProps = {
   href: string;
   children: any;
 }
 
-type LinkTypes = 'primary-link' | 'secondary-link' | 'tertiary-link';
+// type LinkTypes = 'primary-link' | 'secondary-link' | 'tertiary-link';
 
-const LinkButton = ({ href, children }: LinkButtonProps) => {
+// styles['primary-link']
 
-  return (
-    <>
-      <a className={styles['primary-link']} href={href} target="_blank" rel="noreferrer">
-        { children }
-      </a>
-    </>
-  )
-}
+const LinkButton = ({ href, children }: LinkButtonProps) => (
+  <>
+    <a
+      href={href}
+      target="_blank"
+      rel="noreferrer"
+      style={{
+        background: 'linear-gradient(55.58deg, #FF338F 9.41%, #F6AE13 87.82%)',
+      }}
+      className="flex justify-center py-3 my-3 text-cs-white rounded-xl text-xl"
+    >
+      { children }
+    </a>
+  </>
+);
 
 export default LinkButton;
