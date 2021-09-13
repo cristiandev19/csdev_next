f781dfed8071e682963d1de00dc390c3ed999264
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-12 21:46:12 -0500

    updating full width

--- components/ui/atoms/Trophy/Trophy.tsx
+++ components/ui/atoms/Trophy/Trophy.tsx
@@ -11,7 +11,7 @@ const Trophy = ({
   <>
     <a href="https://github.com/ryo-ma/github-profile-trophy">
       <img
-        className="fullwidth"
+        className="w-full"
         src={`https://github-profile-trophy.vercel.app/?username=cristiandev19&row=${row}&column=${col}`}
         alt="cristiandev19"
       />
