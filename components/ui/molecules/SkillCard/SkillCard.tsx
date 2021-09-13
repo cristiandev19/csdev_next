b72d7b73a9a7cd344ebe905f37485729ca9d54d1
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-12 21:24:49 -0500

    changes to styles

--- components/ui/molecules/SkillCard/SkillCard.tsx
+++ components/ui/molecules/SkillCard/SkillCard.tsx
@@ -1,24 +1,33 @@
+import clsx from 'clsx';
 import React from 'react';
 import { DataSkill } from '../../organisms/SkillSection/SkillSection';
-import styles from './SkillCard.module.css';
 
 const SkillCard = ({
   data,
   handleClose,
 }: { data: DataSkill; handleClose: any }) => (
   <div
-    className={styles['skill-card']}
+    className={clsx(
+      'rounded-2xl',
+      'w-4/5',
+      'm-auto',
+      'p-5',
+      'relative',
+    )}
     style={{
       border: `4px ${data.color} solid`,
     }}
   >
     <h3 className="text-center">{data.title}</h3>
     <button
-      className={styles['skill-card__close']}
+      className="border-none absolute"
       onClick={handleClose}
       type="button"
+      style={{ top: '20px', right: '20px', color: data.color }}
     >
-      cerrar
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+      </svg>
     </button>
     <figure className="flex-justify">
       <img src={data.image} alt="" />
