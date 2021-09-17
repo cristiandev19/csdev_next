9a945c9ce4f9333ef8dca6016c6d3a1dbc183ca1
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-17 00:32:47 -0500

    updating gmail icon

--- components/Icons/GmailIcon.tsx
+++ components/Icons/GmailIcon.tsx
@@ -15,8 +15,12 @@ const GmailIcon: FunctionComponent<GmailIconProps> = ({
   height = 24,
 }) => (
   <>
-    <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" />
+    <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg" viewBox="52 42 88 66">
+      <path fill="#4285f4" d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6" />
+      <path fill="#34a853" d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15" />
+      <path fill="#fbbc04" d="M120 48v26l20-15v-8c0-7.42-8.47-11.65-14.4-7.2" />
+      <path fill="#ea4335" d="M72 74V48l24 18 24-18v26L96 92" />
+      <path fill="#c5221f" d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2" />
     </svg>
   </>
 );
