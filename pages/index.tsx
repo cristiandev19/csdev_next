55d850c599620336312e7ad9deee82d7cfc6e5eb
cristiandev <cristiansotomayor.dev@gmail.com>
2021-09-17 00:34:19 -0500

    last index changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
+import React from 'react';
 import MainSection from '../components/ui/organisms/MainSection/MainSection';
 import SkillSection from '../components/ui/organisms/SkillSection/SkillSection';
 import TrophySection from '../components/ui/organisms/TrophySection/TrophySection';
 import MilestoneSection from '../components/ui/organisms/MilestoneSection/MilestoneSection';
-import useWindowSize, { WidthSizesProps } from '../hooks/useWindowSize';
 import HeaderSection from '../components/ui/organisms/HeaderSection/HeaderSection';
+import PageTemplate from '../components/ui/templates/PageTemplate';
 
-const addToolBar: WidthSizesProps<boolean> = {
-  extrasmall: true,
-  small: true,
-  medium: true,
-  large: true,
-  extralarge: false,
-};
+const Home = () => (
+  <PageTemplate title="Inicio | Cristian Sotomayor">
+    <HeaderSection />
 
-const Home = () => {
-  const [showToolbar, setShowToolbar] = useState(true);
+    <div className="container mx-auto sm:px-0 px-8">
+      <MainSection />
 
-  const [size, widthStates] = useWindowSize();
+      <SkillSection />
 
-  useEffect(() => {
-    setShowToolbar(addToolBar[widthStates]);
-  }, [size]);
+      <TrophySection />
 
-  return (
-    <div>
-      <Head>
-        <title>Cristian Sotomayor</title>
-        <meta name="description" content="Personal web made with next" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <HeaderSection />
-
-      <div className="container mx-auto sm:px-0 px-8">
-        <MainSection />
-
-        <SkillSection />
-
-        <TrophySection />
-
-        <MilestoneSection />
-      </div>
+      <MilestoneSection />
     </div>
-  );
-};
+  </PageTemplate>
+);
 
 export default Home;
