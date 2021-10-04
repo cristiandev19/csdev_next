adde462b57161c6d8364092a193b1aa8178fe62a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-04 01:22:53 -0500

    deleting depencencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import MainSection from '../components/ui/organisms/MainSection/MainSection';
 import SkillSection from '../components/ui/organisms/SkillSection/SkillSection';
 import TrophySection from '../components/ui/organisms/TrophySection/TrophySection';
-import MilestoneSection from '../components/ui/organisms/MilestoneSection/MilestoneSection';
 import HeaderSection from '../components/ui/organisms/HeaderSection/HeaderSection';
 import PageTemplate from '../components/ui/templates/PageTemplate';
 
