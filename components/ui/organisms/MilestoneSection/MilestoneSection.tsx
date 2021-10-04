37a49f4cd2f422f2e6ca8ea62a7e960e5dfc7f97
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-04 01:23:11 -0500

    fixing eslint errors

--- components/ui/organisms/MilestoneSection/MilestoneSection.tsx
+++ components/ui/organisms/MilestoneSection/MilestoneSection.tsx
@@ -9,18 +9,20 @@ const MilestoneSection = () => {
       <div>
         <p>
           Empece mi carrera como desarrollador de software en un programa
-          llamado "Peru Rumbo GSoC 2018" organizado por LinuxAtUNI
+          llamado Peru Rumbo GSoC 2018 organizado por LinuxAtUNI
           del cual llegue a ser el Top 1.
         </p>
       </div>
       <div className="flex-justify">
         <iframe
+          title="PeruRumboGSoC2018"
           height="500"
           width="80%"
-          src={urlGnome} />
+          src={urlGnome}
+        />
       </div>
     </section>
-  )
+  );
 };
 
 export default MilestoneSection;
