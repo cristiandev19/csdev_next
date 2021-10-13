4b6bdcf7dd264b38fc281f7524e48b8a2b9aef4a
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-13 00:33:50 -0500

    updating button component

--- components/ui/atoms/Button/Button.tsx
+++ components/ui/atoms/Button/Button.tsx
@@ -1,15 +1,53 @@
 import React, { FunctionComponent } from 'react';
-import styles from './Button.module.css';
 
-type ButtonProps = {
+type ButtonBaseProps = {
   children: React.ReactNode;
+  onClick: () => void;
 };
+interface ButtonProps extends ButtonBaseProps {
+  mode?: ButtonModes;
+}
 
-const Button: FunctionComponent<ButtonProps> = ({ children }) => (
-  <>
-    <button type="button" className={styles['primary-btn']}>
+type ButtonModes = 'raised' | 'icon';
+
+const ButtonIcon: FunctionComponent<ButtonBaseProps> = ({
+  children,
+  onClick,
+}) => {
+  return (
+    <button onClick={onClick} type="button">
       {children}
     </button>
+  );
+};
+
+const ButtonRaised: FunctionComponent<ButtonBaseProps> = ({
+  children,
+  onClick,
+}) => {
+  return (
+    <button
+      type="button"
+      className="py-3 px-7 bg-cs-pink text-white"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+const Button: FunctionComponent<ButtonProps> = ({
+  children,
+  onClick,
+  mode = 'raised',
+}) => (
+  <>
+    {
+      {
+        icon: <ButtonIcon onClick={onClick}>{children}</ButtonIcon>,
+        raised: <ButtonRaised onClick={onClick}>{children}</ButtonRaised>,
+      }[mode]
+    }
   </>
 );
 
