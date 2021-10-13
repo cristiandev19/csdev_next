bc79a1b38411fc3d48ebbaaabd18388501291a65
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-12 23:23:12 -0500

    erasing background

--- components/ui/organisms/HeaderSection/HeaderSection.tsx
+++ components/ui/organisms/HeaderSection/HeaderSection.tsx
@@ -28,7 +28,6 @@ const HeaderSection: VoidFunctionComponent = () => {
   return (
     <nav
       className={clsx(
-        'bg-gray-50',
         'dark:bg-cs-black',
         'p-2',
         'mt-0',
