a7fff5fc77d496d0571220acba390cb36b4d6a44
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-16 00:14:16 -0500

    layout page of home and contact

---
+++ modules/contact/layout.tsx
@@ -0,0 +1,18 @@
+import HeaderSection from 'components/ui/organisms/HeaderSection/HeaderSection';
+import PageTemplate from 'components/ui/templates/PageTemplate';
+import SocialSection from 'modules/contact/sections/SocialSection';
+import React, { VoidFunctionComponent } from 'react';
+
+const ContactLayout: VoidFunctionComponent = () => {
+  return (
+    <PageTemplate title="Contacto | Cristian Sotomayor">
+      <HeaderSection />
+
+      <div className="container mx-auto sm:px-0 px-8 pb-40 pt-40">
+        <SocialSection />
+      </div>
+    </PageTemplate>
+  );
+};
+
+export default ContactLayout;
