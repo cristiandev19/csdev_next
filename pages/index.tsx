87876099ea9f93ff09870d8a344cff376a380c4b
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-15 23:58:13 -0500

    index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import MainSection from '../components/ui/organisms/MainSection/MainSection';
-import SkillSection from '../components/ui/organisms/SkillSection/SkillSection';
-import TrophySection from '../components/ui/organisms/TrophySection/TrophySection';
-import HeaderSection from '../components/ui/organisms/HeaderSection/HeaderSection';
-import PageTemplate from '../components/ui/templates/PageTemplate';
 import { NextPage } from 'next';
-import ShortInformation from '../components/ui/organisms/ShortInformation';
-// import PresentationSection from '../components/ui/organisms/PresentationSection';
+import HomeLayout from 'modules/home/layout';
 
-const Home: NextPage = () => (
-  <PageTemplate title="Inicio | Cristian Sotomayor">
-    <HeaderSection />
-
-    <div className="pt-40 md:pt-0">
-      <MainSection />
-
-      <ShortInformation />
-
-      {/* <PresentationSection /> */}
-
-      <SkillSection />
-
-      <TrophySection />
-
-      {/* <MilestoneSection /> */}
-    </div>
-  </PageTemplate>
-);
+const Home: NextPage = () => <HomeLayout />;
 
 export default Home;
