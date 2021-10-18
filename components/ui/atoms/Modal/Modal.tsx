9d8e86ac0b832e16b1a0fc0171756399ada1551f
cristiandev <cristiansotomayor.dev@gmail.com>
2021-10-17 21:33:04 -0500

    fixing modal

--- components/ui/atoms/Modal/Modal.tsx
+++ components/ui/atoms/Modal/Modal.tsx
@@ -85,15 +85,15 @@ const Modal: FunctionComponent<ModalProps> = ({ open, onClose, children }) => {
                   'shadow-xl',
                   'transform',
                   'transition-all',
-                  'm-0',
                   'sm:my-8',
+                  'm-0',
                   'sm:align-middle',
                   'w-full',
                   'sm:max-w-3xl',
                   'relative',
                 )}
               >
-                <div className="h-screen sm:h-full dark:bg-cs-black sm:rounded-lg bg-white p-4 md:px-8 md:pt-8 md:pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:h-5/6 dark:bg-cs-black sm:rounded-lg bg-white p-4 md:px-8 md:pt-8 md:pb-4 sm:p-6 sm:pb-4">
                   <div>{children}</div>
                 </div>
                 <div
