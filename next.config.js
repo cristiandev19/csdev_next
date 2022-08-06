3a7276c1c11f147e6fbec13f520c37ee04952c7b
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2022-08-06 14:07:47 -0500

    using the new SWC compiler

--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
