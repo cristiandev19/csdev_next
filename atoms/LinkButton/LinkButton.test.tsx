a7144a085d2ec966acd2a60d3ed4885bf6be2e47
csdev19 <cristiansotomayor.dev@gmail.com>
2022-08-14 23:02:26 -0500

    add new tests

--- atoms/LinkButton/LinkButton.test.tsx
+++ atoms/LinkButton/LinkButton.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import LinkButton from './LinkButton';
 
-describe('MainSection', () => {
-  beforeEach(() => {
-    render(<LinkButton href='"hola"'>Child</LinkButton>);
-  });
+describe('Test of the Link button component', () => {
+  it('Should render the link renders the link button with internal mode', () => {
+    const text = 'Child';
+    const href = '/contact';
+    render(<LinkButton href={href}>{text}</LinkButton>);
+    const link = screen.getByText(text);
 
-  it('renders a heading', () => {
-    const child = screen.getByText('Child');
-
-    expect(child).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+    expect(link.getAttribute('href')).toBe(href);
   });
 });
