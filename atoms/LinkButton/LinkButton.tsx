02f7b4afe513d9bf7d304c7cfc886d5b88a9f5ca
csdev19 <cristiansotomayor.dev@gmail.com>
2022-08-14 22:58:31 -0500

    update LinkButton into different components

--- atoms/LinkButton/LinkButton.tsx
+++ atoms/LinkButton/LinkButton.tsx
@@ -1,64 +1,32 @@
-import clsx from 'clsx';
 import React, { FunctionComponent } from 'react';
-import Link from 'next/link';
-import styles from './LinkButton.module.css';
 import { LinkButtonProps } from './LinkButton.types';
+import LinkInternal from '@atoms/LinkButton/LinkInternal';
+import LinkExternal from '@atoms/LinkButton/LinkExternal';
 
 const LinkButton: FunctionComponent<LinkButtonProps> = ({
   href = '',
   children,
   className = '',
   type = 'internal',
-}) => (
-  <>
-    {
+}) => {
+  return (
+    <>
       {
-        internal: (
-          <Link href={href}>
-            <a
-              className={clsx(
-                'transition duration-300 ease-in-out hover:scale-105',
-                'flex',
-                'justify-center',
-                'py-3',
-                'my-3',
-                'text-cs-white',
-                'rounded-xl',
-                'text-xl',
-                styles.gradient1,
-                className,
-              )}
-            >
+        {
+          internal: (
+            <LinkInternal className={className} href={href}>
               {children}
-            </a>
-          </Link>
-        ),
-        external: (
-          <a
-            href={href}
-            target="_blank"
-            rel="noreferrer"
-            className={clsx(
-              'transition duration-300 ease-in-out hover:scale-105',
-              // 'gradient-1',
-              styles.gradient1,
-              'flex',
-              'justify-center',
-              'py-3',
-              'my-3',
-              'text-cs-white',
-              'rounded-xl',
-              'text-xl',
-              'word-break',
-              className,
-            )}
-          >
-            {children}
-          </a>
-        ),
-      }[type]
-    }
-  </>
-);
+            </LinkInternal>
+          ),
+          external: (
+            <LinkExternal className={className} href={href}>
+              {children}
+            </LinkExternal>
+          ),
+        }[type]
+      }
+    </>
+  );
+};
 
 export default LinkButton;
--- atoms/LinkButton/LinkButton.types.ts
+++ atoms/LinkButton/LinkButton.types.ts
@@ -1,8 +1,11 @@
-export type LinkButtonProps = {
-  href?: string;
+export interface LinkBaseProps {
+  href: string;
   className?: string;
   children: React.ReactNode;
+}
+
+export interface LinkButtonProps extends LinkBaseProps {
   type?: LinkType;
-};
+}
 
 export type LinkType = 'internal' | 'external';
