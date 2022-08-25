937ec6f8bfbd92f80d11aadd4f9f6425987dd674
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2022-08-24 21:17:07 -0500

    new constants and use that constants

--- organisms/SocialSection/SocialSection.tsx
+++ organisms/SocialSection/SocialSection.tsx
@@ -6,6 +6,12 @@ import {
   YoutubeIcon,
   TwitterIcon,
 } from '@shared/Icons';
+import {
+  GITHUB_URL,
+  LINKEDIN_URL,
+  TWITTER_URL,
+  YOUTUBE_URL,
+} from '@shared/constants';
 
 const SocialSection: VoidFunctionComponent = () => {
   const handleClick = (url): void => {
@@ -14,35 +20,25 @@ const SocialSection: VoidFunctionComponent = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-8">
-      <HoveredCard onClick={() => handleClick('https://github.com/csdev19')}>
+      <HoveredCard onClick={() => handleClick(GITHUB_URL)}>
         <div className="flex items-center	flex-col p-10">
           <GithubIcon width={80} height={80} />
         </div>
       </HoveredCard>
 
-      <HoveredCard
-        onClick={() =>
-          handleClick('https://www.linkedin.com/in/cristian-sotomayor19/')
-        }
-      >
+      <HoveredCard onClick={() => handleClick(LINKEDIN_URL)}>
         <div className="flex items-center	flex-col p-10">
           <LinkedinIcon width={80} height={80} />
         </div>
       </HoveredCard>
 
-      <HoveredCard
-        onClick={() =>
-          handleClick(
-            'https://www.youtube.com/channel/UCbcKdTfx7bE4bim6yynimZw',
-          )
-        }
-      >
+      <HoveredCard onClick={() => handleClick(YOUTUBE_URL)}>
         <div className="flex items-center	flex-col p-10">
           <YoutubeIcon width={80} height={80} />
         </div>
       </HoveredCard>
 
-      <HoveredCard onClick={() => handleClick('https://twitter.com/csdev19')}>
+      <HoveredCard onClick={() => handleClick(TWITTER_URL)}>
         <div className="flex items-center	flex-col p-10">
           <TwitterIcon width={80} height={80} />
         </div>
--- organisms/TechnologiesSection/TechnologiesSection.tsx
+++ organisms/TechnologiesSection/TechnologiesSection.tsx
@@ -4,6 +4,7 @@ import Section from '@atoms/Section';
 import { DataTechnology } from '@organisms/TechnologiesSection/TechnologiesSection.types';
 import ProgressImg from '@molecules/ProgressImg';
 import TechnologiesModal from '@molecules/TechnologiesModal/TechnologiesModal';
+import Background1 from '@shared/svg/Background1';
 
 const TechnologiesSection: VoidFunctionComponent = () => {
   const { t } = useTranslation();
@@ -15,7 +16,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/git.png',
       color: '#F05133',
-      // percent: 80,
       title: t('tech.git.title'),
       technologies: t('tech.git.tech'),
     },
@@ -24,7 +24,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/mongo.png',
       color: '#70B35E',
-      // percent: 70,
       title: t('tech.mongo.title'),
       technologies: t('tech.mongo.tech'),
     },
@@ -33,7 +32,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/node.png',
       color: '#83CD29',
-      // percent: 90,
       title: t('tech.node.title'),
       technologies: t('tech.node.tech'),
     },
@@ -42,7 +40,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/typescript.png',
       color: '#007ACD',
-      // percent: 90,
       title: t('tech.ts.title'),
       technologies: t('tech.ts.tech'),
     },
@@ -51,7 +48,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/css.png',
       color: '#2965F1',
-      // percent: 70,
       title: t('tech.css.title'),
       technologies: t('tech.css.tech'),
     },
@@ -60,7 +56,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/sass.png',
       color: '#CD6799',
-      // percent: 70,
       title: t('tech.sass.title'),
       technologies: t('tech.sass.tech'),
     },
@@ -69,7 +64,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/sql.png',
       color: '#157EFB',
-      // percent: 80,
       title: t('tech.sql.title'),
       technologies: t('tech.sql.tech'),
     },
@@ -78,7 +72,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/react.png',
       color: '#00D8FF',
-      // percent: 60,
       title: t('tech.react.title'),
       technologies: t('tech.react.tech'),
     },
@@ -87,7 +80,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/vue.png',
       color: '#00C887',
-      // percent: 60,
       title: t('tech.vue.title'),
       technologies: t('tech.vue.tech'),
     },
@@ -96,7 +88,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/angular.png',
       color: '#DD0031',
-      // percent: 80,
       title: t('tech.angular.title'),
       technologies: t('tech.angular.tech'),
     },
@@ -105,7 +96,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/aws.png',
       color: '#FF9900',
-      // percent: 50,
       title: t('tech.aws.title'),
       technologies: t('tech.aws.tech'),
     },
@@ -114,7 +104,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
       stroke: 8,
       image: '/images/figma.png',
       color: '#A259FF',
-      // percent: 25,
       title: t('tech.figma.title'),
       technologies: t('tech.figma.tech'),
     },
@@ -144,7 +133,6 @@ const TechnologiesSection: VoidFunctionComponent = () => {
               stroke={data.stroke}
               color={data.color}
               image={data.image}
-              // percent={data.percent}
               key={data.image}
               onClick={() => handleClick(data)}
             />
@@ -157,121 +145,7 @@ const TechnologiesSection: VoidFunctionComponent = () => {
         open={opened}
       />
       <div className="absolute w-full h-full">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 1203 1110"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M1203 364C1203 565.032 1040.03 728 839 728C637.968 728 475 565.032 475 364C475 162.968 637.968 0 839 0C1040.03 0 1203 162.968 1203 364Z"
-            fill="url(#paint0_linear_127:89)"
-          />
-          <path
-            d="M379 622.5C379 680.766 331.766 728 273.5 728C215.234 728 168 680.766 168 622.5C168 564.234 215.234 517 273.5 517C331.766 517 379 564.234 379 622.5Z"
-            fill="url(#paint1_linear_127:89)"
-          />
-          <path
-            d="M576 826.5C576 880.9 531.9 925 477.5 925C423.1 925 379 880.9 379 826.5C379 772.1 423.1 728 477.5 728C531.9 728 576 772.1 576 826.5Z"
-            fill="url(#paint2_linear_127:89)"
-          />
-          <path
-            d="M99 572.5C99 599.838 76.8381 622 49.5 622C22.1619 622 0 599.838 0 572.5C0 545.162 22.1619 523 49.5 523C76.8381 523 99 545.162 99 572.5Z"
-            fill="url(#paint3_linear_127:89)"
-          />
-          <path
-            d="M458 1060.5C458 1087.84 435.838 1110 408.5 1110C381.162 1110 359 1087.84 359 1060.5C359 1033.16 381.162 1011 408.5 1011C435.838 1011 458 1033.16 458 1060.5Z"
-            fill="url(#paint4_linear_127:89)"
-          />
-          <path
-            d="M99 875.5C99 889.031 88.031 900 74.5 900C60.969 900 50 889.031 50 875.5C50 861.969 60.969 851 74.5 851C88.031 851 99 861.969 99 875.5Z"
-            fill="url(#paint5_linear_127:89)"
-          />
-          <path
-            d="M273 925.5C273 939.031 262.031 950 248.5 950C234.969 950 224 939.031 224 925.5C224 911.969 234.969 901 248.5 901C262.031 901 273 911.969 273 925.5Z"
-            fill="url(#paint6_linear_127:89)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint1_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint2_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint3_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint4_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint5_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-            <linearGradient
-              id="paint6_linear_127:89"
-              x1="-2.05032e-05"
-              y1="853.106"
-              x2="1024.63"
-              y2="92.1992"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF338F" />
-              <stop offset="1" stopColor="#F6AE13" />
-            </linearGradient>
-          </defs>
-        </svg>
+        <Background1 />
       </div>
     </Section>
   );
