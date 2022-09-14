0aff42a8051145848d0b7b9773d02b07d3edab73
Cristian Sotomayor <cristiansotomayor.dev@gmail.com>
2022-09-13 22:46:30 -0500

    new stories

--- atoms/Section/Section.stories.tsx
+++ atoms/Section/Section.stories.tsx
@@ -1,16 +1,16 @@
-import { ComponentStory } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Section from './Section';
 
 export default {
   title: 'Atoms/Section',
   component: Section,
-};
+} as ComponentMeta<typeof Section>;
 
 const Template: ComponentStory<typeof Section> = (args) => (
   <Section {...args} />
 );
 
-export const Normal: ComponentStory<typeof Section> = Template.bind({});
+export const Normal = Template.bind({});
 
 Normal.args = {
   children: 'Normal section',
